f6f28c84a7c9d74ada6b8fe1c834cce19dca158d
camelrush <shinya.pyonkici@gmail.com>
2024-05-09 22:17:44 +0900

    取得したデータを、指定した期間に応じた単位の平均で返却するように変更

--- aws_lambda/src/moistureDataApi/lambda_function.py
+++ aws_lambda/src/moistureDataApi/lambda_function.py
@@ -1,8 +1,8 @@
 import boto3
-# from boto3.dynamodb.conditions import Key
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
-
+from collections import defaultdict
+import statistics
 
 def lambda_handler(event, context):
 
@@ -14,26 +14,32 @@ def lambda_handler(event, context):
 
     if span == 'today':
         starttm = '{0:%Y%m%d}'.format(now) + '000000'
+        group_format = '%Y%m%d%H%M'
 
     elif span == '3days':
         starttm = '{0:%Y%m%d}'.format(now + timedelta(days=-3)) + '000000'
+        group_format = '%Y%m%d%H'
 
     elif span == '7days':
         starttm = '{0:%Y%m%d}'.format(now + timedelta(days=-7)) + '000000'
+        group_format = '%Y%m%d%H'
 
     elif span == '1month':
         starttm = '{0:%Y%m%d}'.format(
             now + relativedelta(months=-1)) + '000000'
+        group_format = '%Y%m%d'
 
     elif span == '3months':
         starttm = '{0:%Y%m%d}'.format(
             now + relativedelta(months=-3)) + '000000'
+        group_format = '%Y%m%d'
 
     elif span == '1year':
         starttm = '{0:%Y%m%d}'.format(now + relativedelta(years=-1)) + '000000'
+        group_format = '%Y%m'
 
     else:
-        starttm = '{0:%Y%m%d}'.format(now) + '000000'
+        return ''
 
     print(starttm)
 
@@ -45,7 +51,24 @@ def lambda_handler(event, context):
             ":dts": 'dev_room',
             ":val": int(starttm)
         },
-        ScanIndexForward=True,  # 降順でソート
+        ScanIndexForward=True
     )
 
-    return queryData['Items']
+    grouped_data = defaultdict(lambda: {'moisture': [], 'humidity': [], 'temperature': []})
+    for item in queryData['Items']:
+        group_key = datetime.strptime(str(item['dt_val']), '%Y%m%d%H%M%S').strftime(group_format)
+        grouped_data[group_key]['moisture'].append(item['moisture'])
+        grouped_data[group_key]['humidity'].append(item['humidity'])
+        grouped_data[group_key]['temperature'].append(item['temperature'])
+
+    # 平均値を計算し、リスト形式でデータを返す
+    averaged_data = []
+    for key, values in grouped_data.items():
+        averaged_data.append({
+            'dt_val': key,
+            'moisture': round(statistics.mean(values['moisture']), 1),
+            'humidity': round(statistics.mean(values['humidity']), 1),
+            'temperature': round(statistics.mean(values['temperature']), 1)
+        })
+
+    return averaged_data
